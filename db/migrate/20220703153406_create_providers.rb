12d74bac2e5d31895fc8785949bc117776bf4e95
Daniel Täsch <accounts@datae.de>
2022-07-03 17:37:54 +0200

    rails g scaffold provider name email iban

---
+++ db/migrate/20220703153406_create_providers.rb
@@ -0,0 +1,11 @@
+class CreateProviders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :providers do |t|
+      t.string :name
+      t.string :email
+      t.string :iban
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_03_152250) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_03_153406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_03_152250) do
     t.string "email"
     t.string "iban"
     t.date "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "iban"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
